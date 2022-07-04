4c06dc2fbf1f375cd3d272d3a1fb401338da21eb
Loukei <weber781015+bitbucket@gmail.com>
2022-07-04 17:42:15 +0800

    YT playlistItems requests

--- code/youtube_api.py
+++ code/youtube_api.py
@@ -1,11 +1,17 @@
 import requests
 
-
-def get_playlist_Info(apikey:str,video_id:str):
+def get_playlistItems(apikey:str,playlistId:str):
+    """ get playlistItem info 
+    - [Doc](https://developers.google.com/youtube/v3/docs/playlistItems)
     """
-    #TODO
-    """
-    pass
+    url:str = "https://youtube.googleapis.com/youtube/v3/playlistItems"
+    payload:dict = {
+        "part": "snippet",
+        "playlistId": playlistId,
+        "key": apikey,
+        "Accept": "application/json",
+    }
+    return requests.get(url, params=payload)
 
 def get_videoInfo(apikey:str,video_id:str) -> requests.Response:
     """ get YT video info
@@ -20,5 +26,8 @@ def get_videoInfo(apikey:str,video_id:str) -> requests.Response:
     return requests.get(url, params=payload)
 
 if __name__ == "__main__":
-    # test here
+    "test here"
+    playlistid_1:str = "PLQgT9G4SVR-LQfix29B8zYvM-Pk-adPG_" # "freeCodeCamp C++ courses"
+    reply = get_playlistItems(apikey="",playlistId=playlistid_1)
+    print(reply.text)
     pass
