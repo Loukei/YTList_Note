5661298f718615dd96fa8174b5626c04b6f69e64
Loukei <weber781015+bitbucket@gmail.com>
2022-07-04 17:08:12 +0800

    clear redudant code in models.py

--- code/models/models.py
+++ code/models/models.py
@@ -1,20 +1,2 @@
-"""
-Define the models for Youtube Video and Youtube Playlist
-"""
-
-import json
-from ytvideo import YTVideo, yt_video_from_dict
-
-def test_video_model(file_path:str):
-    print(f'path = {file_path}')
-    with open(file = file_path, mode='r', encoding='utf8') as file:
-        video:YTVideo = yt_video_from_dict(json.load(file)["items"][0])
-        # print(video)
-        print(video.id)
-        print(video.content_details.duration)
-    pass
-
 if __name__ == "__main__":
-    test_fp:str = "./code/output/bC7o8P_Ste4.json"
-    test_video_model(file_path = test_fp)
     pass
